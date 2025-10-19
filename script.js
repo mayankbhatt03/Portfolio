c4fa7587120b0a6675e5452d33ac46c2f399e0d1
Mayank Bhatt <mayankbhatt310@gmail.com>
2025-10-19 17:25:55 +0530

    Update and rename body.css to script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
+// Hide navbar automatically when a link is clicked
+document.querySelectorAll('.nav-links a').forEach(link => {
+  link.addEventListener('click', () => {
+    navLinks.classList.remove('active');
+  });
+});
